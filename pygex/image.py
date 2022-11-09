7cb2c61bd4191d9f34ea2a2b503c840e2f1dce07
TeaCondemns <tea.condemns@gmail.com>
2022-11-09 21:56:32 +0300

    Update image.py
    Code refactoring

--- pygex/image.py
+++ pygex/image.py
@@ -2,44 +2,53 @@ from pygame.image import frombuffer as pg_image_frombuffer, tostring as pg_image
 from PIL import Image as PillowImage, ImageFilter as PillowImageFilter
 from pygame.surface import Surface, SurfaceType
 from pygame.draw import rect as pg_draw_rect
-from pygame import pg_SRCALPHA
 from typing import Sequence
 
 
-def pillow_to_pygame_image(img: PillowImage):
-    return pg_image_frombuffer(img.tobytes(), img.size, 'RGBA')
+def pillow_to_pygame(source: PillowImage):
+    return pg_image_frombuffer(source.tobytes(), source.size, 'RGBA')
 
 
-def pygame_to_pillow_image(img: SurfaceType):
-    return PillowImage.frombytes('RGBA', img.get_size(), pg_image_tostring(img, 'RGBA'))
+def pygame_to_pillow(source: SurfaceType):
+    return PillowImage.frombytes('RGBA', source.get_size(), pg_image_tostring(source, 'RGBA'))
 
 
-def set_blur(img: SurfaceType, intensity: int):
-    return pillow_to_pygame_image(pygame_to_pillow_image(img).filter(PillowImageFilter.GaussianBlur(intensity)))
+def get_blurred(source: SurfaceType, intensity: int):
+    return pillow_to_pygame(pygame_to_pillow(source).filter(PillowImageFilter.GaussianBlur(intensity)))
 
 
-def set_round_corners(img: SurfaceType, radius: int | tuple[int, int, int, int] | Sequence):
+def get_cutout_by_mask(source: SurfaceType, mask: SurfaceType):
+    """
+    The cutout color on the mask is black (#000000)
+
+    :param source: original Surface
+
+    :param mask: mask for cutout
+    """
+    new_source = source.copy()
+
+    for x in range(0, source.get_size()[0]):
+        for y in range(0, source.get_size()[1]):
+            if mask.get_at((x, y)) == (0, 0, 0, 255):
+                new_source.set_at((x, y), 0x00000000)
+
+    return new_source
+
+
+def get_rounded_corners(source: SurfaceType, radius: int | tuple[int, int, int, int] | Sequence):
     """
     Rounding the borders of image (Surface)
 
-    :param img: source image
+    :param source: source image
     :param radius: value by which the corners will be rounded: `radius` or `top_left, top_right, bottom_left, bottom_tight`
     """
     radius = (radius,) if isinstance(radius, int) else (-1, *radius)
-    size = img.get_size()
-    mask = Surface(size)
+    mask = Surface(source.get_size())
     mask.fill(0x000000)
 
-    pg_draw_rect(mask, 0xffffff, (0, 0, *size), 0, *radius)
+    pg_draw_rect(mask, 0xffffff, (0, 0, *source.get_size()), 0, *radius)
 
-    new_img = Surface(size, img.get_flags() | pg_SRCALPHA, 32)
-
-    for x in range(0, img.get_size()[0]):
-        for y in range(0, img.get_size()[1]):
-            if mask.get_at((x, y)) == (255, 255, 255, 255):
-                new_img.set_at((x, y), img.get_at((x, y)))
-
-    return new_img
+    return get_cutout_by_mask(source, mask)
 
 
-__all__ = 'pillow_to_pygame_image', 'pygame_to_pillow_image', 'set_blur', 'set_round_corners'
+__all__ = 'pillow_to_pygame', 'pygame_to_pillow', 'get_blurred', 'get_cutout_by_mask', 'get_rounded_corners'
