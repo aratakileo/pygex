c31766a4b38deda3287ebec59a105da6e31de161
TeaCondemns <tea.condemns@gmail.com>
2022-11-21 19:37:44 +0300

    Update mouse.py
    - New mouse features & interfaces

--- pygex/mouse.py
+++ pygex/mouse.py
@@ -1,15 +1,22 @@
+from pygame.mouse import get_pos as pg_mouse_get_pos, set_pos as pg_mouse_set_pos
 from pygame.constants import MOUSEBUTTONDOWN, MOUSEBUTTONUP, MOUSEWHEEL
-from pygame.mouse import get_pos as pg_mouse_get_pos
-from pygame.event import Event
+from pygame.display import get_window_size
+from pygame.event import Event, set_grab
 from pygex.input import Input
+from typing import Sequence
 
 
 class Mouse:
+    FLAG_NO_BORDERS = 1 << 0
+    FLAG_CONFINED = 1 << 1
+    FLAG_CAPTURED = 1 << 2
+
     def __init__(self):
         global _active_mouse
         _active_mouse = self
 
         self._last_pos = pg_mouse_get_pos()
+        self.flags = 0
 
         self.button_statuses = [Input.NOT_PRESSED] * 3
         self.wheel = (0, 0)
@@ -90,7 +97,35 @@ class Mouse:
     def rel(self):
         return self.relx, self.rely
 
-    get_pos = pg_mouse_get_pos
+    @property
+    def pos(self):
+        return pg_mouse_get_pos()
+
+    @pos.setter
+    def pos(self, value: Sequence[float]):
+        pg_mouse_set_pos(value)
+
+    @property
+    def x(self):
+        return pg_mouse_get_pos()[0]
+
+    @x.setter
+    def x(self, value: float):
+        pg_mouse_set_pos(value, pg_mouse_get_pos()[1])
+
+    @property
+    def y(self):
+        return pg_mouse_get_pos()[1]
+
+    @y.setter
+    def y(self, value: float):
+        pg_mouse_set_pos(pg_mouse_get_pos()[0], value)
+
+    def add_flags(self, flags: int):
+        self.flags |= flags
+
+    def remove_flags(self, flags: int):
+        self.flags &= ~flags
 
     def process_event(self, e: Event):
         if e.type == MOUSEWHEEL:
@@ -106,6 +141,21 @@ class Mouse:
                         self.button_statuses[k] = statuses[i]
 
     def flip(self):
+        set_grab(bool(self.flags & Mouse.FLAG_CONFINED))
+
+        if self.flags & Mouse.FLAG_NO_BORDERS:
+            if self.x > get_window_size()[0]:
+                self.x = 0
+
+            if self.x < 0:
+                self.x = get_window_size()[0]
+
+            if self.y > get_window_size()[1]:
+                self.y = 0
+
+            if self.y < 0:
+                self.y = get_window_size()[1]
+
         for i in range(len(self.button_statuses)):
             if self.button_statuses[i] == Input.DOWN:
                 self.button_statuses[i] = Input.HOLD
@@ -114,6 +164,9 @@ class Mouse:
 
         self.wheel = (0, 0)
         self._last_pos = pg_mouse_get_pos()
+
+        if self.flags & Mouse.FLAG_CAPTURED:
+            pg_mouse_set_pos(get_window_size()[0] / 2, get_window_size()[1] / 2)
 
 
 _active_mouse: Mouse | None = None
