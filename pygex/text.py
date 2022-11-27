4ee71dfdc2a27806df1bdf6a97c70d22fa6ab974
TeaCondemns <tea.condemns@gmail.com>
2022-11-27 17:59:56 +0300

    Update text.py

--- pygex/text.py
+++ pygex/text.py
@@ -5,7 +5,7 @@ from pygame.font import FontType, get_init, init, Font
 def render_text(
         text: any,
         alpha_color: colorValue,
-        font_or_size: FontType | int = None,
+        font_or_size: FontType | int = 20,
         antialias=True
 ):
     if not get_init():
@@ -15,8 +15,6 @@ def render_text(
 
     if isinstance(font_or_size, int):
         font = Font(None, font_or_size)
-    elif font_or_size is None:
-        font = Font(None, 20)
 
     return font.render(text.__str__(), antialias, to_pygame_alpha_color(alpha_color))
 
