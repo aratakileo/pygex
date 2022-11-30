3c40b31679d33da634692732eff6b8001b2887a5
TeaCondemns <tea.condemns@gmail.com>
2022-11-30 21:19:03 +0300

    Update text.py
    - new extended function for render text (multiline, lines limit, line spacing, align)

--- pygex/text.py
+++ pygex/text.py
@@ -1,10 +1,20 @@
 from pygex.color import colorValue, to_pygame_alpha_color
 from pygame.font import FontType, get_init, init, Font
+from pygex.image import AlphaSurface
+from pygame.rect import Rect
+from typing import Sequence
+
+DEFAULT_FONT_SIZE = 20
+
+ALIGN_LEFT = 0
+ALIGN_RIGHT = 1
+ALIGN_CENTER = 2
+ALIGN_BLOCK = 3
 
 _font_buffer = {}
 
 
-def get_pygame_font(font_or_size: FontType | int = 20):
+def get_pygame_font(font_or_size: FontType | int = DEFAULT_FONT_SIZE):
     if not get_init():
         init()
 
@@ -17,12 +27,117 @@ def get_pygame_font(font_or_size: FontType | int = 20):
     return font_or_size
 
 
-def render_text(text, alpha_color: colorValue, font_or_size: FontType | int = 20, antialias=True):
-    return get_pygame_font(font_or_size).render(text.__str__(), antialias, to_pygame_alpha_color(alpha_color))
-
-
-def get_text_size(text, font_or_size: FontType | int = 20):
+def get_text_size(text, font_or_size: FontType | int = DEFAULT_FONT_SIZE):
     return get_pygame_font(font_or_size).size(text.__str__())
 
 
-__all__ = 'get_pygame_font', 'render_text', 'get_text_size'
+def render_text(text, color: colorValue, font_or_size: FontType | int = DEFAULT_FONT_SIZE, antialias=True):
+    return get_pygame_font(font_or_size).render(text.__str__(), antialias, to_pygame_alpha_color(color))
+
+
+def render_wrapped_text(
+        text,
+        color: colorValue,
+        rect: Sequence | Rect,
+        font_or_size: FontType | int = DEFAULT_FONT_SIZE,
+        align=ALIGN_LEFT,
+        line_spacing=0,
+        lines: int = ...,
+        antialias=True
+):
+    text = text.__str__().rstrip()
+    color = to_pygame_alpha_color(color)
+
+    if not text or lines <= 0:
+        return None
+
+    font = get_pygame_font(font_or_size)
+    textw = font.size(text)[0]
+
+    if (
+            align == ALIGN_LEFT or align == ALIGN_BLOCK and ' ' not in text.strip()
+    ) and '\n' not in text and textw <= rect[2]:
+        return font.render(text, antialias, color)
+
+    output_surface = AlphaSurface(rect[2:])
+
+    if '\n' not in text and textw <= rect[2] and align in (ALIGN_RIGHT, ALIGN_CENTER):
+        if align == ALIGN_RIGHT:
+            output_surface.blit(font.render(text, antialias, color), (rect[2] - textw, 0))
+        else:
+            output_surface.blit(font.render(text, antialias, color), ((rect[2] - textw) / 2, 0))
+
+        return output_surface
+
+    space_char_width, charh = font.size(' ')
+
+    charh += line_spacing
+
+    rendered_width = 0
+    line_num = 0
+
+    for line in text.split('\n'):
+        for word in line.rstrip().split(' '):
+            if not word:
+                rendered_width += space_char_width
+                continue
+
+            wordw = font.size(word)[0]
+
+            if rendered_width + wordw >= rect[2] and rendered_width > 0:
+                line_num += 1
+
+                if (line_num + 1) * charh - rect[3] > charh or lines is not ... and line_num == lines:
+                    break
+
+                rendered_width = 0
+
+            if rendered_width == 0 and wordw > rect[2]:
+                chars_in_line = int(rect[2] / (wordw / len(word)))
+                chars_rendered = 0
+
+                while chars_rendered < len(word):
+                    chars_for_render = min(len(word) - chars_rendered, chars_in_line)
+
+                    chars_surface = font.render(
+                        word[chars_rendered: chars_rendered + chars_for_render],
+                        antialias,
+                        color
+                    )
+
+                    output_surface.blit(chars_surface, (0, line_num * charh))
+
+                    rendered_width = chars_surface.get_width()
+                    chars_rendered += chars_for_render
+                    line_num += 1
+
+                    if (line_num + 1) * charh - rect[3] > charh or lines is not ... and line_num == lines:
+                        break
+
+                line_num -= 1
+            else:
+                output_surface.blit(font.render(word, antialias, color), (rendered_width, line_num * charh))
+                rendered_width += wordw
+
+            rendered_width += space_char_width
+
+        rendered_width = 0
+        line_num += 1
+
+        if line_num * charh - rect[3] > charh:
+            break
+
+    return output_surface
+
+
+__all__ = (
+    'DEFAULT_FONT_SIZE',
+    'ALIGN_LEFT',
+    'ALIGN_RIGHT',
+    'ALIGN_CENTER',
+    'ALIGN_BLOCK',
+    'get_pygame_font',
+    'get_text_size',
+    'render_text',
+    'render_wrapped_text'
+)
