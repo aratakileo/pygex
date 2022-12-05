16b7ee62bad955793927d03ba6ba82c4740901d3
TeaCondemns <tea.condemns@gmail.com>
2022-12-05 14:42:06 +0300

    Update __init__.py
    - code refactored

--- pygex/__init__.py
+++ pygex/__init__.py
@@ -1,7 +1,7 @@
 from pygex.window import Window, get_window
+from pygex.input import Input, get_input
+from pygex.mouse import get_mouse
 from pygex.gui.toast import Toast
-from pygex.input import *
-from pygex.mouse import *
 
 
-__all__ = 'Window', 'get_window', 'Toast', 'Input', 'get_input', 'Mouse', 'get_mouse'
+__all__ = 'Window', 'get_window', 'Toast', 'Input', 'get_input', 'get_mouse'
