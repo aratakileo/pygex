579860826113a566578d83143338ed03c91958ae
TeaCondemns <tea.condemns@gmail.com>
2022-12-08 21:40:30 +0300

    Update __init__.py

--- pygex/__init__.py
+++ pygex/__init__.py
@@ -1,8 +1,5 @@
 from pygex.window import Window, get_window
-from pygex.input import Input, get_input
-from pygex.mouse import get_mouse
 from pygex.gui.toast import Toast
+from pygex.input import *
+from pygex.mouse import *
 from pygex import color
-
-
-__all__ = 'Window', 'get_window', 'Toast', 'Input', 'get_input', 'get_mouse', 'color'
