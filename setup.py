277a1c5d19e88b1dbd21f8dd82c5b7e45d46fd0c
TeaCondemns <tea.condemns@gmail.com>
2022-12-10 22:50:01 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ except ImportError:
 
 setup(
     name='pygex',
-    version='0.2.dev1',
+    version='0.2.dev2',
     packages=['pygex', 'pygex.gui'],
     url='https://github.com/teacondemns/pygex',
     install_requires=['Pillow>=9.0', 'pygame>=2.0'],
