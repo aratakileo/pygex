aaa478610843e25ff7f84c4ce1b4d4c07c46df23
TeaCondemns <tea.condemns@gmail.com>
2022-12-11 17:04:58 +0300

    Update __init__.py

--- pygex/gui/__init__.py
+++ pygex/gui/__init__.py
@@ -1,6 +1,6 @@
 from pygex.gui.view import GRAVITY_LEFT, GRAVITY_TOP, GRAVITY_RIGHT, GRAVITY_BOTTOM, GRAVITY_CENTER_HORIZONTAL
 from pygex.gui.textview import ALIGN_LEFT, ALIGN_RIGHT, ALIGN_CENTER, ALIGN_BLOCK, TextView
-from pygex.gui.view import GRAVITY_CENTER_VERTICAL, SIZE_WRAP_CONTENT
+from pygex.gui.view import GRAVITY_CENTER_VERTICAL, SIZE_WRAP_CONTENT, SIZE_MATCH_PARENT
 from pygex.gui.drawable import ColorDrawable, GradientDrawable
 from pygex.gui.toast import Toast
 from pygex.gui.hint import Hint
