12a8f5092cd7b2bdf6d83c9e9009746ff6f947d9
TeaCondemns <tea.condemns@gmail.com>
2023-02-27 18:57:39 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ setup(
     version='0.3.2',
     packages=['pygex', 'pygex.gui', 'pygex.gui.drawable'],
     url='https://github.com/teacondemns/pygex',
-    install_requires=['Pillow>=9.0', 'pygame>=2.0.1'],
+    install_requires=['pygame>=2.0.1'],
     license='MIT',
     author='Tea Condemns',
     author_email='tea.condemns@gmail.com',
