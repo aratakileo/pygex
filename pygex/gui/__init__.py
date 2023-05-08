df806bc65de9f8cfb6091c26dd93f04a492226ca
TeaCondemns <tea.condemns@gmail.com>
2023-05-08 21:57:56 +0300

    view.py update

--- pygex/gui/__init__.py
+++ pygex/gui/__init__.py
@@ -2,7 +2,7 @@ from pygex.gui.view import GRAVITY_LEFT, GRAVITY_TOP, GRAVITY_RIGHT, GRAVITY_BOT
 from pygex.gui.view import GRAVITY_CENTER_VERTICAL, SIZE_WRAP_CONTENT, SIZE_MATCH_PARENT, VISIBILITY_INVISIBLE
 from pygex.gui.linearlayout import LinearLayout, ORIENTATION_HORIZONTAL, ORIENTATION_VERTICAL
 from pygex.gui.textview import ALIGN_LEFT, ALIGN_RIGHT, ALIGN_CENTER, ALIGN_BLOCK, TextView
-from pygex.gui.view import VISIBILITY_VISIBLE, VISIBILITY_GONE
+from pygex.gui.view import VISIBILITY_VISIBLE, VISIBILITY_GONE, NO_PADDING
 from pygex.gui.buttonview import ButtonView
 from pygex.gui.toast import Toast
 from pygex.gui.hint import Hint
