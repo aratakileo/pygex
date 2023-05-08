afcf138b70b821cb5b2f77abb71874dc3a9ae5df
TeaCondemns <tea.condemns@gmail.com>
2023-05-08 20:39:34 +0300

    info.py update

--- pygex/__init__.py
+++ pygex/__init__.py
@@ -1,5 +1,6 @@
 from pygex.window import Window, get_window
 from pygex.gui.toast import Toast
+from pygex.info import ver
 from pygex.input import *
 from pygex.mouse import *
 from pygex import color
