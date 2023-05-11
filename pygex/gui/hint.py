5bbda2a1b4bcafe66aa4263c91f9b7e281cfbb74
TeaCondemns <tea.condemns@gmail.com>
2023-05-11 19:31:33 +0300

    Update hint.py

--- pygex/gui/hint.py
+++ pygex/gui/hint.py
@@ -1,10 +1,10 @@
 from pygex.color import TYPE_COLOR, as_rgba, to_readable_color, COLOR_BLACK
 from pygex.text import render_text, DEFAULT_FONT_SIZE
+from pygex.broker import get_window, get_mouse
 from pygame.draw import rect as draw_rect
 from pygex.interface import Renderable
 from pygame.surface import SurfaceType
 from pygex.image import AlphaSurface
-from pygex.broker import get_window
 from pygame.rect import RectType
 from pygame.font import FontType
 from typing import Sequence
@@ -55,11 +55,14 @@ class Hint(Renderable):
 
     def provide_show(
             self,
-            anchor_rect_or_point: Sequence[float | int] | RectType,
+            anchor_rect_or_pos: Sequence[float | int] | RectType = ...,
             bounds_in: Sequence[float | int] | RectType = ...
     ):
-        if len(anchor_rect_or_point) == 2:
-            anchor_rect_or_point = *anchor_rect_or_point, 0, 0
+        if anchor_rect_or_pos is ...:
+            anchor_rect_or_pos = get_mouse()
+        
+        if len(anchor_rect_or_pos) == 2:
+            anchor_rect_or_pos = *anchor_rect_or_pos, 0, 0
 
         if bounds_in is ...:
             bounds_in = get_window().surface.get_bounding_rect()
@@ -73,7 +76,7 @@ class Hint(Renderable):
         textw, texth = self._text_surface_buffer.get_size()
         boxw, boxh = textw + self.padding * 2, texth + self.padding * 2
 
-        box_x, box_y = anchor_rect_or_point[:2]
+        box_x, box_y = anchor_rect_or_pos[:2]
 
         is_gravity_left_of_center = self.gravity & GRAVITY_LEFT_OF_CENTER
         is_gravity_right_of_center = self.gravity & GRAVITY_RIGHT_OF_CENTER
@@ -82,32 +85,32 @@ class Hint(Renderable):
         is_gravity_under_center = self.gravity & GRAVITY_UNDER_CENTER
 
         if self.gravity & GRAVITY_RIGHT:
-            box_x += anchor_rect_or_point[2] - textw - self.padding * 2
+            box_x += anchor_rect_or_pos[2] - textw - self.padding * 2
         elif self.gravity & GRAVITY_CENTER_HORIZONTAL:
-            box_x += (anchor_rect_or_point[2] - textw) / 2 - self.padding * 2
+            box_x += (anchor_rect_or_pos[2] - textw) / 2 - self.padding * 2
         elif is_gravity_left_of_center:
             box_x -= textw + self.padding * 2
         elif is_gravity_right_of_center:
-            box_x += anchor_rect_or_point[2]
+            box_x += anchor_rect_or_pos[2]
 
         if is_gravity_right_of_center and box_x + boxw > bounds_in[0] + bounds_in[2]:
-            box_x = anchor_rect_or_point[0] - textw - self.padding * 2
+            box_x = anchor_rect_or_pos[0] - textw - self.padding * 2
         elif is_gravity_left_of_center and box_x <= 0:
-            box_x = anchor_rect_or_point[0] + anchor_rect_or_point[2]
+            box_x = anchor_rect_or_pos[0] + anchor_rect_or_pos[2]
 
         if self.gravity & GRAVITY_BOTTOM:
-            box_y += anchor_rect_or_point[3] - texth - self.padding * 2
+            box_y += anchor_rect_or_pos[3] - texth - self.padding * 2
         elif self.gravity & GRAVITY_CENTER_VERTICAL:
-            box_y += (anchor_rect_or_point[3] - texth) / 2 - self.padding * 2
+            box_y += (anchor_rect_or_pos[3] - texth) / 2 - self.padding * 2
         elif is_gravity_over_center:
             box_y -= texth + self.padding * 2
         elif is_gravity_under_center:
-            box_y += anchor_rect_or_point[3]
+            box_y += anchor_rect_or_pos[3]
 
         if is_gravity_under_center and box_y + boxh > bounds_in[1] + bounds_in[3]:
-            box_y = anchor_rect_or_point[1] - texth - self.padding * 2
+            box_y = anchor_rect_or_pos[1] - texth - self.padding * 2
         elif is_gravity_over_center and box_y <= 0:
-            box_y = anchor_rect_or_point[1] + anchor_rect_or_point[3]
+            box_y = anchor_rect_or_pos[1] + anchor_rect_or_pos[3]
 
         if self.strict_fit_in_bounds:
             if box_x + boxw > bounds_in[0] + bounds_in[2]:
