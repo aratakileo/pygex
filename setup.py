c986bfb433093abbc240223f7b2b29b191f0e59b
TeaCondemns <tea.condemns@gmail.com>
2023-05-11 18:29:36 +0300

    Update setup.py
    Moving from pygame to pygame-ce

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ setup(
     version='0.3.3',
     packages=['pygex', 'pygex.gui', 'pygex.gui.drawable', 'pygex.prefab'],
     url='https://github.com/teacondemns/pygex',
-    install_requires=['pygame>=2.0.1'],
+    install_requires=['pygame-ce>=2.2.1'],
     license='MIT',
     author='Tea Condemns',
     author_email='tea.condemns@gmail.com',
