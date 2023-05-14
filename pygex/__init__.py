daef362b98f5c7ce60770c80f019201bdc2865fe
TeaCondemns <tea.condemns@gmail.com>
2023-05-14 21:20:18 +0300

    Update __init__.py

--- pygex/__init__.py
+++ pygex/__init__.py
@@ -2,9 +2,9 @@ from pygex.broker import get_input, get_mouse, get_window
 from pygex.debugpanel import DebugPanel
 from pygex.gui.toast import Toast
 from pygex.window import Window
-from pygex.input import Input
 from pygex.mouse import Mouse
 from pygex.info import ver
+from pygex.input import *
 from pygex import color
 from pygex import draw
 from pygex import gui
