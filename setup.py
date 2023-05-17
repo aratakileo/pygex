ab98c924b76120dcb6d5531a37bffb39695e643e
TeaCondemns <tea.condemns@gmail.com>
2023-05-17 20:00:14 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,15 +3,35 @@ try:
 except ImportError:
     from distutils.core import setup
 
+PACKAGE_NAME = PACKAGE_ROOT = 'pygex'
+PACKAGE_VER = open(f'{PACKAGE_ROOT}/core/info.py').read().split('\n')[0].split(' = ')[-1][1:-1]
+
+packages = [PACKAGE_ROOT]
+package_data = {
+    PACKAGE_ROOT: ['resource/*/*']
+}
+
+
+def add_package(package_name: str):
+    packages.append(PACKAGE_ROOT + '.' + package_name)
+    package_data[PACKAGE_ROOT].append(package_name.replace('.', '/') + '/*.pyi')
+
+
+add_package('gui')
+add_package('gui.drawable')
+add_package('core')
+
+
 setup(
-    name='pygex',
-    version='0.3.4',
-    packages=['pygex', 'pygex.gui', 'pygex.gui.drawable', 'pygex.core'],
-    package_data={'pygex.core': ['broker.pyi']},
-    url='https://github.com/teacondemns/pygex',
+    name=PACKAGE_NAME,
+    version=PACKAGE_VER,
+    packages=packages,
+    package_data=package_data,
+    url=f'https://github.com/teacondemns/{PACKAGE_NAME}',
     install_requires=['pygame-ce>=2.2.1'],
     license='MIT',
     author='Tea Condemns',
     author_email='tea.condemns@gmail.com',
-    description='Extended library for pygame users.'
+    description='An extensive module of various tools and tools for creating a modern graphical interface '
+                'for pygame-ce users.'
 )
